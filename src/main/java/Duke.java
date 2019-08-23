adfcfe08803e2ee1b8d69ef9d02c208a569f2869
kuromono <clouderext@gmail.com>
2019-08-23 14:36:18 +0800

    Level-3: Mark as Done (Fixed)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,26 +17,48 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println(line);
 
-        ArrayList<String> input_list = new ArrayList<String>();
+        ArrayList<Task> task_list = new ArrayList<Task>();
 
         // INPUT LOOP
         Scanner read_line = new Scanner(System.in);
         String input = "";
         while(!input.equals("bye"))
         {
+            // read input & tokenize
             input = read_line.nextLine();
+            String[] tokenized_input = input.split(" ");
 
-            if (!input.equals("list"))
+            if (input.equals("list")) // Lists Everything
             {
-                input_list.add(input);
-                System.out.println(line + "\nadded: " + input + "\n" + line);
-            }
-            else
-            {
-                for(int i = 0; i < input_list.size(); i += 1)
+                if (task_list.size() != 0) // Set task list boundary
                 {
-                    System.out.println(i + ". " + input_list.get(i));
+                    for(int i = 0; i < task_list.size(); i += 1)
+                    {
+                        Task t = task_list.get(i);
+                        System.out.println((i + 1) + "." + t.getStatusIcon() + " " + t.description);
+                    }
                 }
+                else
+                {
+                    System.out.println(line + "\nThere is nothing in the list!\n" + line);
+                }
+            }
+            else if(tokenized_input[0].equals("done")) // Mark Tasks as Done
+            {
+                int input_num = Integer.parseInt(tokenized_input[1]);
+                if (input_num > 0 && input_num <= task_list.size()) // Set task list boundary
+                {
+                    task_list.get(input_num - 1).isDone = true;
+                }
+                System.out.println(line + "\nNice! I've marked this task as done:");
+                System.out.println("  " + task_list.get(input_num - 1).getStatusIcon() + " " + task_list.get(input_num - 1).description);
+                System.out.println(line);
+            }
+            else // Add new tasks
+            {
+                Task t = new Task(input);
+                task_list.add(t);
+                System.out.println(line + "\nadded: " + input + "\n" + line);
             }
 
         }
@@ -45,7 +67,8 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
 
-        // CLOSE SCANNER
+        // Close scanner & Exit
         read_line.close();
+        System.exit(0);
     }
 }
