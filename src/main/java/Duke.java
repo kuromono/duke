05786116a4327dcf8a56528bd7157d77d553d825
kuromono <clouderext@gmail.com>
2019-08-30 14:55:10 +0800

    Revert "Group similar input[1] checks outside to reduce code length"
    This reverts commit b7d172591c62df395d7c7d649f6b1539c8be2a2d.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -44,96 +44,97 @@ public class Duke {
                 if (input.equals("list")) {
                     // List everything
                     TaskLogic.list(task_list);
-                } else {
-
+                } else if (tokenized_input[0].equals("done")) {
+                    // Marks task as Done
                     if (tokenized_input.length == 1) {
-                        throw new DukeException("The value/index of <" + tokenized_input[0] + "> cannot be empty.");
+                        System.err.println("The index of <done> cannot be empty.");
+                        continue;
+                    } else if (!TaskLogic.IntValidator(tokenized_input[1])) {
+                        System.err.println("The index of <done> is not a valid integer.");
+                        continue;
                     }
 
-                    if (tokenized_input[0].equals("done")) {
-                        // Marks task as Done
-                        if (!TaskLogic.IntValidator(tokenized_input[1])) {
-                            System.err.println("The index of <done> is not a valid integer.");
+                    TaskLogic.done(task_list, Integer.parseInt(tokenized_input[1]));
+                    FileLogic.update_file(file, task_list);
+                } else if (tokenized_input[0].equals("delete")) {
+                    if (tokenized_input.length == 1) {
+                        System.err.println("The index of <delete> cannot be empty.");
+                        continue;
+                    } else if (!TaskLogic.IntValidator(tokenized_input[1])) {
+                        System.err.println("The index of <delete> is not a valid integer.");
+                        continue;
+                    }
+
+                    TaskLogic.delete(task_list, Integer.parseInt(tokenized_input[1]));
+                    FileLogic.update_file(file, task_list);
+                } else if (tokenized_input[0].equals("find")) {
+                    // Search task based on keyword
+                    if (tokenized_input.length == 1) {
+                        System.err.println("The value of <find> cannot be empty.");
+                        continue;
+                    }
+
+                    TaskLogic.find_task(task_list, tokenized_input[1]);
+                } else {
+                    // Adds new Tasks
+                    String desc;
+                    if (tokenized_input[0].equals("todo")) {
+                        if (input.strip().length() < 5) {
+                            System.err.println("The description of a <todo> cannot be empty.");
                             continue;
                         }
 
-                        TaskLogic.done(task_list, Integer.parseInt(tokenized_input[1]));
+                        TaskLogic.add_task(task_list, TaskLogic.make_task("todo", input.substring(5), ""));
                         FileLogic.update_file(file, task_list);
-                    } else if (tokenized_input[0].equals("delete")) {
-                        if (!TaskLogic.IntValidator(tokenized_input[1])) {
-                            System.err.println("The index of <delete> is not a valid integer.");
+                    } else if (tokenized_input[0].equals("deadline")) {
+                        if (tokenized_input.length < 3) {
+                            System.err.println("The description of a <deadline> cannot be empty.");
+                            System.err.println("Format: deadline <description> /by DD/MM/YYYY HHMM");
                             continue;
                         }
 
-                        TaskLogic.delete(task_list, Integer.parseInt(tokenized_input[1]));
-                        FileLogic.update_file(file, task_list);
-                    } else if (tokenized_input[0].equals("find")) {
-                        // Search task based on keyword
+                        tokenized_input = input.substring(9).split(" /by ");
+                        if (tokenized_input.length < 2) {
+                            System.err.println("Incorrect format. Format should be: deadline <description> /by DD/MM/YYYY HHMM");
+                            continue;
+                        } else if (!TaskLogic.DateValidator(tokenized_input[1])) {
+                            System.err.println("Date format must be DD/MM/YYYY HHMM, E.g. 02/12/2019 1800");
+                            continue;
+                        }
 
-                        TaskLogic.find_task(task_list, tokenized_input[1]);
+                        TaskLogic.add_task(task_list, TaskLogic.make_task("deadline", tokenized_input[0], tokenized_input[1]));
+                        FileLogic.update_file(file, task_list);
+                    } else if (tokenized_input[0].equals("event")) {
+                        if (tokenized_input.length < 3) {
+                            System.err.println("The description of a <event> cannot be empty.");
+                            System.err.println("Format: event <description> /at DD/MM/YYYY HHMM");
+                            continue;
+                        }
+
+                        tokenized_input = input.substring(6).split(" /at ");
+                        if (tokenized_input.length < 2) {
+                            System.err.println("Incorrect format. Format should be: event <description> /at DD/MM/YYYY HHMM");
+                            continue;
+                        } else if (!TaskLogic.DateValidator(tokenized_input[1])) {
+                            System.err.println("Date format must be DD/MM/YYYY HHMM, E.g. 02/12/2019 1800");
+                            continue;
+                        }
+
+                        TaskLogic.add_task(task_list, TaskLogic.make_task("event", tokenized_input[0], tokenized_input[1]));
+                        FileLogic.update_file(file, task_list);
+                    } else if (input.equals("reset")) { // Reset the list & delete file - for debug use
+                        task_list = TaskLogic.reset(task_list);
+                        FileLogic.reset_file(file);
+                    } else if (input.equals("bye")) {
+                        // Close scanner and exit programs
+                        read_line.close();
+                        exitDuke();
                     } else {
-                        // Adds new Tasks
-                        String desc;
-                        if (tokenized_input[0].equals("todo")) {
-
-                            TaskLogic.add_task(task_list, TaskLogic.make_task("todo", input.substring(5), ""));
-                            FileLogic.update_file(file, task_list);
-                        } else if (tokenized_input[0].equals("deadline")) {
-                            if (tokenized_input.length < 3) {
-                                System.err.println("The description of a <deadline> cannot be empty.");
-                                System.err.println("Format: deadline <description> /by DD/MM/YYYY HHMM");
-                                continue;
-                            }
-
-                            tokenized_input = input.substring(9).split(" /by ");
-                            if (tokenized_input.length < 2) {
-                                System.err.println("Incorrect format. Format should be: deadline <description> /by DD/MM/YYYY HHMM");
-                                continue;
-                            } else if (!TaskLogic.DateValidator(tokenized_input[1])) {
-                                System.err.println("Date format must be DD/MM/YYYY HHMM, E.g. 02/12/2019 1800");
-                                continue;
-                            }
-
-                            TaskLogic.add_task(task_list, TaskLogic.make_task("deadline", tokenized_input[0], tokenized_input[1]));
-                            FileLogic.update_file(file, task_list);
-                        } else if (tokenized_input[0].equals("event")) {
-                            if (tokenized_input.length < 3) {
-                                System.err.println("The description of a <event> cannot be empty.");
-                                System.err.println("Format: event <description> /at DD/MM/YYYY HHMM");
-                                continue;
-                            }
-
-                            tokenized_input = input.substring(6).split(" /at ");
-                            if (tokenized_input.length < 2) {
-                                System.err.println("Incorrect format. Format should be: event <description> /at DD/MM/YYYY HHMM");
-                                continue;
-                            } else if (!TaskLogic.DateValidator(tokenized_input[1])) {
-                                System.err.println("Date format must be DD/MM/YYYY HHMM, E.g. 02/12/2019 1800");
-                                continue;
-                            }
-
-                            TaskLogic.add_task(task_list, TaskLogic.make_task("event", tokenized_input[0], tokenized_input[1]));
-                            FileLogic.update_file(file, task_list);
-                        } else if (input.equals("reset")) { // Reset the list & delete file - for debug use
-                            task_list = TaskLogic.reset(task_list);
-                            FileLogic.reset_file(file);
-                        } else if (input.equals("bye")) {
-                            // Close scanner and exit programs
-                            read_line.close();
-                            exitDuke();
-                        } else {
-                            throw new DukeException("\n"
-                                    + "The system currently only supports the following formats:\n"
-                                    + "  todo <description>\n"
-                                    + "  deadline <description> /by <date>\n"
-                                    + "  event <description> /at <date>\n"
-                                    + "  list\n"
-                                    + "  done <index>\n"
-                                    + "  delete <index>\n"
-                                    + "  find <keyword>\n"
-                                    + "  reset"
-                            );
-                        }
+                        throw new DukeException("\n"
+                                + "The system currently only supports the following formats:\n"
+                                + "  todo <description>\n"
+                                + "  deadline <description> /by <date>\n"
+                                + "  event <description> /at <date>");
                     }
                 }
             } catch (DukeException | IOException | ArrayIndexOutOfBoundsException e) {
